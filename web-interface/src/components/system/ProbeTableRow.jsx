af85a28e5f075394f19cd512d132de7368a6573f
Lennart Koopmann <lennart@0x58ed.com>
2019-04-19 14:54:39 -0500

    abbreviate channels list in system probe status

--- web-interface/src/components/system/ProbeTableRow.jsx
+++ web-interface/src/components/system/ProbeTableRow.jsx
@@ -8,7 +8,7 @@ class ProbesTableRow extends Reflux.Component {
         super(props);
     }
 
-    _decideStatus(isInLoop) {
+    static _decideStatus(isInLoop) {
         if (isInLoop) {
             return (
                 <i className="fas fa-check-square text-success"></i>
@@ -20,17 +20,25 @@ class ProbesTableRow extends Reflux.Component {
         }
     }
 
+    // Limit to 13 total channels and abbreviate if it's more.
+    static _printChannels(channels) {
+        if (channels.length > 13) {
+            return channels.slice(0,13).toString() + " ..."
+        } else {
+            return channels.toString();
+        }
+    }
+
     render() {
         const probe = this.props.probe;
 
         return (
             <tr>
                 <td>{probe.name}</td>
-                <td>{this._decideStatus(probe.is_in_loop)}</td>
+                <td>{ProbesTableRow._decideStatus(probe.is_in_loop)}</td>
                 <td>{probe.class_name}</td>
                 <td>{probe.network_interface}</td>
-                <td>{probe.current_channel}</td>
-                <td>{probe.channels.toString()}</td>
+                <td title={probe.channels.toString()}>{ProbesTableRow._printChannels(probe.channels)}</td>
                 <td>{numeral(probe.total_frames).format('0,0')}</td>
             </tr>
         )
