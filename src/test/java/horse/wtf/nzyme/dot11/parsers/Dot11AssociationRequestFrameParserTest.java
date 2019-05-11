5af46f54779bdb196ab1dcf2b6c9824e205329de
Lennart Koopmann <lennart@0x58ed.com>
2019-05-11 16:31:38 -0500

    prep disassoc tests

---
+++ src/test/java/horse/wtf/nzyme/dot11/parsers/Dot11AssociationRequestFrameParserTest.java
@@ -0,0 +1,14 @@
+package horse.wtf.nzyme.dot11.parsers;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Dot11AssociationRequestFrameParserTest extends FrameParserTest {
+
+    @Test
+    public void testDoParse() {
+
+    }
+
+}
