b1c2feb065ae598166e8cfe8495bf58abeeb64b3
Lennart Koopmann <lennart@0x58ed.com>
2019-05-18 14:54:58 -0500

    test for assoc-resp refused handler

--- src/test/java/horse/wtf/nzyme/dot11/handlers/Dot11AssociationResponseFrameHandlerTest.java
+++ src/test/java/horse/wtf/nzyme/dot11/handlers/Dot11AssociationResponseFrameHandlerTest.java
@@ -20,7 +20,7 @@ import static org.testng.Assert.*;
 public class Dot11AssociationResponseFrameHandlerTest extends FrameHandlerTest {
 
     @Test
-    public void testDoHandle() throws MalformedFrameException, IllegalRawDataException {
+    public void testDoHandleSuccessResponse() throws MalformedFrameException, IllegalRawDataException {
         Nzyme nzyme = new MockNzyme();
         Dot11Probe probe = new Dot11MockProbe(nzyme, CONFIG_STANDARD, new Statistics());
         LoopbackUplink loopback = new LoopbackUplink();
@@ -42,4 +42,29 @@ public class Dot11AssociationResponseFrameHandlerTest extends FrameHandlerTest {
         assertEquals(n.getAdditionalFields().get("_response_string"), "success");
         assertEquals(n.getAdditionalFields().get("_subtype"), "assoc-resp");
     }
+
+    @Test
+    public void testDoHandleFailResponse() throws MalformedFrameException, IllegalRawDataException {
+        Nzyme nzyme = new MockNzyme();
+        Dot11Probe probe = new Dot11MockProbe(nzyme, CONFIG_STANDARD, new Statistics());
+        LoopbackUplink loopback = new LoopbackUplink();
+        probe.registerUplink(loopback);
+
+        Dot11AssociationResponseFrame frame = new Dot11AssociationResponseFrameParser(new MetricRegistry())
+                .parse(Frames.ASSOC_RESP_FAILED_1_PAYLOAD, Frames.ASSOC_RESP_FAILED_1_HEADER, META_NO_WEP);
+
+        new Dot11AssociationResponseFrameHandler(probe).doHandle(frame);
+
+        Notification n = loopback.getLastNotification();
+
+        assertEquals(n.getAdditionalFields().size(), 6);
+        assertEquals(n.getMessage(),"88:96:4e:4d:77:80 answered association request from 5c:77:76:d3:26:45. Response: REFUSED (1)");
+        assertEquals(n.getAdditionalFields().get("_channel"), 1);
+        assertEquals(n.getAdditionalFields().get("_transmitter"), "88:96:4e:4d:77:80");
+        assertEquals(n.getAdditionalFields().get("_destination"), "5c:77:76:d3:26:45");
+        assertEquals(n.getAdditionalFields().get("_response_code"), (short)1);
+        assertEquals(n.getAdditionalFields().get("_response_string"), "refused");
+        assertEquals(n.getAdditionalFields().get("_subtype"), "assoc-resp");
+    }
+
 }
