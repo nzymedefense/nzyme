3be3b19792de3ba64b57d8d16aa9acfa50c8c35e
Lennart Koopmann <lennart@0x58ed.com>
2019-05-19 15:57:08 -0500

    deauth handler test

---
+++ src/test/java/horse/wtf/nzyme/dot11/handlers/Dot11DeauthenticationFrameHandlerTest.java
@@ -0,0 +1,47 @@
+package horse.wtf.nzyme.dot11.handlers;
+
+import com.codahale.metrics.MetricRegistry;
+import horse.wtf.nzyme.MockNzyme;
+import horse.wtf.nzyme.Nzyme;
+import horse.wtf.nzyme.dot11.MalformedFrameException;
+import horse.wtf.nzyme.dot11.frames.Dot11DeauthenticationFrame;
+import horse.wtf.nzyme.dot11.parsers.Dot11DeauthenticationFrameParser;
+import horse.wtf.nzyme.dot11.parsers.Frames;
+import horse.wtf.nzyme.dot11.probes.Dot11MockProbe;
+import horse.wtf.nzyme.dot11.probes.Dot11Probe;
+import horse.wtf.nzyme.notifications.Notification;
+import horse.wtf.nzyme.notifications.uplinks.misc.LoopbackUplink;
+import horse.wtf.nzyme.statistics.Statistics;
+import org.pcap4j.packet.IllegalRawDataException;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Dot11DeauthenticationFrameHandlerTest extends FrameHandlerTest {
+
+    @Test
+    public void testDoHandle() throws MalformedFrameException, IllegalRawDataException {
+        Nzyme nzyme = new MockNzyme();
+        Dot11Probe probe = new Dot11MockProbe(nzyme, CONFIG_STANDARD, new Statistics());
+        LoopbackUplink loopback = new LoopbackUplink();
+        probe.registerUplink(loopback);
+
+        Dot11DeauthenticationFrame frame = new Dot11DeauthenticationFrameParser(new MetricRegistry())
+                .parse(Frames.DEAUTH_1_PAYLOAD, Frames.DEAUTH_1_HEADER, META_NO_WEP);
+
+        new Dot11DeauthenticationFrameHandler(probe).doHandle(frame);
+
+        Notification n = loopback.getLastNotification();
+
+        assertEquals(n.getAdditionalFields().size(), 7);
+        assertEquals(n.getMessage(), "Deauth: Transmitter b0:93:5b:1d:c8:f1 is deauthenticating e4:b2:fb:27:50:15 from BSSID b0:93:5b:1d:c8:f1 (Class 2 frame received from nonauthenticated STA)");
+        assertEquals(n.getAdditionalFields().get("_channel"), 1);
+        assertEquals(n.getAdditionalFields().get("_transmitter"), "b0:93:5b:1d:c8:f1");
+        assertEquals(n.getAdditionalFields().get("_destination"), "e4:b2:fb:27:50:15");
+        assertEquals(n.getAdditionalFields().get("_bssid"), "b0:93:5b:1d:c8:f1");
+        assertEquals(n.getAdditionalFields().get("_reason_code"), (short)6);
+        assertEquals(n.getAdditionalFields().get("_reason_string"), "Class 2 frame received from nonauthenticated STA");
+        assertEquals(n.getAdditionalFields().get("_subtype"), "deauth");
+    }
+
+}
