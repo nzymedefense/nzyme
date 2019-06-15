a260d9b4f3dce2e802400938815f63fd748b6916
Lennart Koopmann <lennart@0x58ed.com>
2019-06-15 18:23:09 -0500

    first alert test

---
+++ src/test/java/horse/wtf/nzyme/alerts/AlertTest.java
@@ -0,0 +1,87 @@
+/*
+ *  This file is part of nzyme.
+ *
+ *  nzyme is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nzyme is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with nzyme.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package horse.wtf.nzyme.alerts;
+
+import horse.wtf.nzyme.MockNzyme;
+import horse.wtf.nzyme.configuration.BanditFingerprintDefinition;
+import horse.wtf.nzyme.configuration.Dot11NetworkDefinition;
+import horse.wtf.nzyme.dot11.Dot11MetaInformation;
+import horse.wtf.nzyme.dot11.probes.Dot11MockProbe;
+import horse.wtf.nzyme.dot11.probes.Dot11ProbeConfiguration;
+import horse.wtf.nzyme.statistics.Statistics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlertTest {
+
+    protected static final Dot11MetaInformation META_NO_WEP = new Dot11MetaInformation(false, 100, 2400, 1, 0L, false);
+
+    protected static final Map<String, BanditFingerprintDefinition> BANDITS_STANDARD = new HashMap<String, BanditFingerprintDefinition>(){{
+        put("ec398735dc99267d453908d81bfe06ce04cfa2573d0b9edf1d940f0dbf850a9c",
+                BanditFingerprintDefinition.create(
+                        "ec398735dc99267d453908d81bfe06ce04cfa2573d0b9edf1d940f0dbf850a9c",
+                        new ArrayList<String>(){{
+                            add("WiFi Pineapple Nano or Tetra (PineAP)");
+                            add("spacehuhn/esp8266_deauther (attack frames)");
+                        }}));
+        put("535afea1f1656375a991e28ce919d412fd9863a01f1b0b94fcff8a83ed8fcb83",
+                BanditFingerprintDefinition.create(
+                        "535afea1f1656375a991e28ce919d412fd9863a01f1b0b94fcff8a83ed8fcb83",
+                        new ArrayList<String>(){{
+                            add("WiFi Pineapple Nano or Tetra (PineAP)");
+                        }}));
+    }};
+
+    protected Dot11MockProbe buildMockProbe(Map<String, BanditFingerprintDefinition> bandits) {
+        return buildMockProbe(bandits, new MockNzyme());
+    }
+
+    protected Dot11MockProbe buildMockProbe(Map<String, BanditFingerprintDefinition> bandits, MockNzyme nzyme) {
+        return new Dot11MockProbe(nzyme, Dot11ProbeConfiguration.create(
+                "test-probe-1",
+                Collections.emptyList(),
+                "nzyme-testng-1",
+                "foo",
+                Collections.emptyList(),
+                0,
+                "foo",
+                new ArrayList<Dot11NetworkDefinition>(){{
+                    add(Dot11NetworkDefinition.create(
+                            "WTF",
+                            new ArrayList<String>(){{
+                                add("00:c0:ca:95:68:3b");
+                            }},
+                            new ArrayList<Integer>() {{
+                                add(1);
+                                add(6);
+                                add(11);
+                            }},
+                            new ArrayList<String>() {{
+                                add("WPA1-EAM-PSK-CCMP");
+                                add("WPA2-EAM-PSK-CCMP");
+                            }})
+                    );
+                }},
+                BANDITS_STANDARD
+        ), new Statistics());
+    }
+
+}
--- src/test/java/horse/wtf/nzyme/alerts/CryptoChangeBeaconAlertTest.java
+++ src/test/java/horse/wtf/nzyme/alerts/CryptoChangeBeaconAlertTest.java
@@ -1,37 +1,102 @@
 package horse.wtf.nzyme.alerts;
 
+import horse.wtf.nzyme.MockNzyme;
+import horse.wtf.nzyme.Subsystem;
+import horse.wtf.nzyme.dot11.probes.Dot11MockProbe;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
-public class CryptoChangeBeaconAlertTest {
+public class CryptoChangeBeaconAlertTest extends AlertTest {
 
     @Test
-    public void testGetMessage() {
+    public void testAlertStandard() {
+        CryptoChangeBeaconAlert a = CryptoChangeBeaconAlert.create(
+                "wtf",
+                "00:c0:ca:95:68:3b",
+                "WPA2-EAM-PSK-CCMP",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        // Wait a little to make lastSeen() assertions work.
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) { /* noop */ }
+
+        assertEquals(a.getEncounteredSecurity(), "WPA2-EAM-PSK-CCMP");
+        assertEquals(a.getSSID(), "wtf");
+        assertEquals(a.getBSSID(), "00:c0:ca:95:68:3b");
+        assertEquals(a.getMessage(), "SSID [wtf] was advertised with unexpected security settings [WPA2-EAM-PSK-CCMP].");
+        assertEquals(a.getType(), Alert.Type.CRYPTO_CHANGE_BEACON);
+        assertEquals(a.getSubsystem(), Subsystem.DOT_11);
+        assertEquals(a.getFrameCount(), 1);
+        assertFalse(a.getLastSeen().isAfterNow());
+        assertTrue(a.getLastSeen().isBeforeNow());
+        assertNotNull(a.getDocumentationLink());
+        assertNotNull(a.getFalsePositives());
+        assertNotNull(a.getDescription());
+
+        CryptoChangeBeaconAlert a2 = CryptoChangeBeaconAlert.create(
+                "wtf",
+                "00:c0:ca:95:68:3b",
+                "WPA2-EAM-PSK-CCMP",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        assertTrue(a.sameAs(a2));
+
+        CryptoChangeBeaconAlert a3 = CryptoChangeBeaconAlert.create(
+                "wtfoooked",
+                "00:c0:ca:95:68:3b",
+                "WPA2-EAM-PSK-CCMP",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        CryptoChangeBeaconAlert a4 = CryptoChangeBeaconAlert.create(
+                "wtf",
+                "00:c0:ca:95:68:3b",
+                "WPA2-EAM-PSK-CCMP-TKIP",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        CryptoChangeBeaconAlert a5 = CryptoChangeBeaconAlert.create(
+                "wtf",
+                "00:c0:ca:95:68:4b",
+                "WPA2-EAM-PSK-CCMP",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        assertFalse(a.sameAs(a3));
+        assertFalse(a.sameAs(a4));
+        assertFalse(a.sameAs(a5));
     }
 
-    @Test
-    public void testGetType() {
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAlertHiddenSSID1() {
+        CryptoChangeBeaconAlert.create(
+                null,
+                "00:c0:ca:95:68:3b",
+                "WPA2-EAM-PSK-CCMP",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
     }
 
-    @Test
-    public void testGetSSID() {
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAlertHiddenSSID2() {
+        CryptoChangeBeaconAlert.create(
+                "",
+                "00:c0:ca:95:68:3b",
+                "WPA2-EAM-PSK-CCMP",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
     }
 
-    @Test
-    public void testGetBSSID() {
-    }
-
-    @Test
-    public void testGetEncounteredSecurity() {
-    }
-
-    @Test
-    public void testSameAs() {
-    }
-
-    @Test
-    public void testCreate() {
-    }
-    
 }
