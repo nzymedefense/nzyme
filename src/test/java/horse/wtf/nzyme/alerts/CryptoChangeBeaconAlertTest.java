810c8ae1991a418d86459a36ee47b0594ff93dda
Lennart Koopmann <lennart@0x58ed.com>
2019-06-15 14:34:59 -0500

    alerts tests skeleton

---
+++ src/test/java/horse/wtf/nzyme/alerts/CryptoChangeBeaconAlertTest.java
@@ -0,0 +1,37 @@
+package horse.wtf.nzyme.alerts;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CryptoChangeBeaconAlertTest {
+
+    @Test
+    public void testGetMessage() {
+    }
+
+    @Test
+    public void testGetType() {
+    }
+
+    @Test
+    public void testGetSSID() {
+    }
+
+    @Test
+    public void testGetBSSID() {
+    }
+
+    @Test
+    public void testGetEncounteredSecurity() {
+    }
+
+    @Test
+    public void testSameAs() {
+    }
+
+    @Test
+    public void testCreate() {
+    }
+    
+}
