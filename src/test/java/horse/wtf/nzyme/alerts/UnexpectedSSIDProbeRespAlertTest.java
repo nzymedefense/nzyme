7511889de211d440ee04dfed30a70c2af9442072
Lennart Koopmann <lennart@0x58ed.com>
2019-06-24 20:02:36 -0500

    unexpected ssid alert tests

---
+++ src/test/java/horse/wtf/nzyme/alerts/UnexpectedSSIDProbeRespAlertTest.java
@@ -0,0 +1,94 @@
+package horse.wtf.nzyme.alerts;
+
+import horse.wtf.nzyme.Subsystem;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class UnexpectedSSIDProbeRespAlertTest extends AlertTest {
+
+    @Test
+    public void testAlertStandard() {
+        UnexpectedSSIDProbeRespAlert a = UnexpectedSSIDProbeRespAlert.create(
+                "wtf",
+                "00:c0:ca:95:68:3b",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        // Wait a little to make lastSeen() assertions work.
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) { /* noop */ }
+
+        assertEquals(a.getSSID(), "wtf");
+        assertEquals(a.getMessage(), "Our BSSID [00:c0:ca:95:68:3b] advertised unexpected SSID [wtf] with probe response frame.");
+        assertEquals(a.getType(), Alert.Type.UNEXPECTED_PROBERESP_SSID);
+        assertEquals(a.getSubsystem(), Subsystem.DOT_11);
+        assertEquals(a.getFrameCount(), 1);
+        assertFalse(a.getLastSeen().isAfterNow());
+        assertTrue(a.getLastSeen().isBeforeNow());
+        assertFalse(a.getFirstSeen().isAfterNow());
+        assertTrue(a.getFirstSeen().isBeforeNow());
+        assertNotNull(a.getDocumentationLink());
+        assertNotNull(a.getFalsePositives());
+        assertNotNull(a.getDescription());
+
+        UnexpectedSSIDProbeRespAlert a2 = UnexpectedSSIDProbeRespAlert.create(
+                "wtf",
+                "00:c0:ca:95:68:3b",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        assertTrue(a.sameAs(a2));
+
+        UnexpectedSSIDProbeRespAlert a3 = UnexpectedSSIDProbeRespAlert.create(
+                "wtfDIFF",
+                "00:c0:ca:95:68:3b",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        UnexpectedSSIDProbeRespAlert a4 = UnexpectedSSIDProbeRespAlert.create(
+                "wtf",
+                "0a:c0:ca:95:68:3b",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        assertFalse(a.sameAs(a3));
+        assertFalse(a.sameAs(a4));
+
+        UnexpectedBSSIDProbeRespAlert a6 = UnexpectedBSSIDProbeRespAlert.create(
+                "wtf",
+                "00:c0:ca:95:68:4b",
+                "00:c0:ca:95:68:4b",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+
+        assertFalse(a.sameAs(a6));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAlertHiddenSSID1() {
+        UnexpectedSSIDProbeRespAlert.create(
+                null,
+                "00:c0:ca:95:68:3b",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAlertHiddenSSID2() {
+        UnexpectedSSIDProbeRespAlert.create(
+                "",
+                "00:c0:ca:95:68:3b",
+                META_NO_WEP,
+                buildMockProbe(BANDITS_STANDARD)
+        );
+    }
+
+}
