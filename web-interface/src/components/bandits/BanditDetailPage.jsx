273a59d497f8433aa3a9b26dee718ef4ef7ed30a
Lennart Koopmann <lennart@0x58ed.com>
2020-01-26 20:49:47 -0600

    bandit detail page

--- web-interface/src/components/bandits/BanditDetailPage.jsx
+++ web-interface/src/components/bandits/BanditDetailPage.jsx
@@ -3,6 +3,8 @@ import Reflux from 'reflux';
 import BanditsActions from "../../actions/BanditsActions";
 import BanditsStore from "../../stores/BanditsStore";
 import LoadingSpinner from "../misc/LoadingSpinner";
+import BeaconRate from "../networks/details/BeaconRate";
+import moment from "moment";
 
 class BanditDetailPage extends Reflux.Component {
 
@@ -34,6 +36,43 @@ class BanditDetailPage extends Reflux.Component {
                 <div className="row">
                     <div className="col-md-12">
                         <h1>Bandit <em>{bandit.name}</em></h1>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-3">
+                        <dl>
+                            <dt>Created at:</dt>
+                            <dd>{moment(bandit.created_at).format()}</dd>
+                        </dl>
+                    </div>
+
+                    <div className="col-md-3">
+                        <dl>
+                            <dt>Updated at:</dt>
+                            <dd>{moment(bandit.updated_at).format()}</dd>
+                        </dl>
+                    </div>
+
+                    <div className="col-md-6">
+                        <dl>
+                            <dt>ID:</dt>
+                            <dd>{bandit.uuid}</dd>
+                        </dl>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <hr />
+
+                        <h3>Description</h3>
+
+                        <div className="alert alert-dark">
+                            {bandit.description.split('\n').map((item, key) => {
+                                return <span key={key}>{item}<br/></span>
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
