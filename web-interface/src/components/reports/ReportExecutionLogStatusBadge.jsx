a997a6d6cb8612b8d08fa85262634d30285706f8
Lennart Koopmann <lennart@0x58ed.com>
2021-10-14 22:20:17 -0500

    remove unreachable code

--- web-interface/src/components/reports/ReportExecutionLogStatusBadge.jsx
+++ web-interface/src/components/reports/ReportExecutionLogStatusBadge.jsx
@@ -6,10 +6,8 @@ class ReportExecutionLogStatusBadge extends React.Component {
         switch(this.props.status) {
             case "SUCCESS":
                 return <span className="badge badge-success">Success</span>
-                break;
             case "ERROR":
                 return <span className="badge badge-danger">Error</span>
-                break;
             default:
                 return <span className="badge badge-warning">Unknown</span>
         }
