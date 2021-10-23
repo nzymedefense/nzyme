6fb50235be74470b41b78ce843dc312598b9fdf4
Lennart Koopmann <lennart@0x58ed.com>
2021-10-22 23:39:36 -0500

    Clean up asset loading structure
    fixes #451

--- web-interface/src/index.js
+++ web-interface/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
+import './bootstrap4.css';
 import './index.css';
+import './fonts.css';
+import './fontawesome.css'
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
