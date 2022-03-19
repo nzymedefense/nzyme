5bdcbb6dd6d847f02df1366a3e77474c83f79e67
Lennart Koopmann <lennart@0x58ed.com>
2022-03-19 16:59:29 -0500

    fix test

--- src/test/java/horse/wtf/nzyme/alerts/UnexpectedFingerprintBeaconAlertTest.java
+++ src/test/java/horse/wtf/nzyme/alerts/UnexpectedFingerprintBeaconAlertTest.java
@@ -45,7 +45,7 @@ public class UnexpectedFingerprintBeaconAlertTest extends AlertTest {
                 DateTime.now(),
                 "wtf",
                 "ec398735dc99267d453908d81bfe06ce04cfa2573d0b9edf1d940f0dbf850a9c",
-                "00:c0:ca:95:68:3e",
+                "00:c0:ca:95:68:3b",
                 1,
                 1000,
                 -50,
