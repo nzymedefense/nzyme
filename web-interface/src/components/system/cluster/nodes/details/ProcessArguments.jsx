382a3596115ac1dd70fd70d4c4ca7b9091375a8f
Lennart Koopmann <lennart@0x58ed.com>
2023-01-28 11:34:18 -0600

    More work on node details page #768

--- web-interface/src/components/system/cluster/nodes/details/ProcessArguments.jsx
+++ web-interface/src/components/system/cluster/nodes/details/ProcessArguments.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ProcessArguments(props) {
+
+  const [expanded, setExpanded] = useState(false)
+
+  const onExpandClick = function () {
+    setExpanded(!expanded);
+  }
 
   if (!props.arguments || props.arguments.length === 0) {
     return <i>[none]</i>
   } else {
-    if (props.arguments.length > 350) {
-      return <code>{props.arguments.slice(0, 350)}</code>
-    } else {
+    if (props.arguments.length <= 350 || expanded) {
       return <code>{props.arguments}</code>
+    } else {
+      return (
+          <div className="node-process-arguments">
+            <code>{props.arguments.slice(0, 350)}</code>{' '}
+            <button className="btn btn-link" onClick={onExpandClick}>[expand]</button>
+          </div>
+      )
     }
   }
 
