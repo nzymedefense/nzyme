nzymedefense/nzyme

commit eca926a3f68ab6075165287e42f50f6c455d59b6

DB-backed health indicators #772

src/main/java/app/nzyme/core/rest/resources/system/CryptoResource.java
@@ -7,7 +7,6 @@ import app.nzyme.plugin.rest.security.RESTSecured;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import app.nzyme.core.NzymeNode;
-import app.nzyme.core.crypto.PGPKeyFingerprint;
 import app.nzyme.core.rest.responses.crypto.CryptoMetricsResponse;
 import app.nzyme.core.rest.responses.crypto.CryptoResponse;
 import app.nzyme.core.rest.responses.crypto.PGPKeyResponse;
@@ -38,11 +37,6 @@ public class CryptoResource {
     @Path("summary")
     public Response summary() {
         Map<String, PGPKeyResponse> fingerprints = Maps.newHashMap();
-        List<String> uniqueFingerprints = Lists.newArrayList();
-        for (PGPKeyFingerprint fp : nzyme.getCrypto().getPGPKeysByNode()) {
-            fingerprints.put(fp.node(), PGPKeyResponse.create(fp.node(), fp.fingerprint(), fp.createdAt()));
-            uniqueFingerprints.add(fp.fingerprint());
-        }
 
         Map<UUID, TimerSnapshot> encryption = nzyme.getClusterManager().findMetricTimer(
                 MetricExternalName.PGP_ENCRYPTION_TIMER.database_label
@@ -131,9 +125,8 @@ public class CryptoResource {
 
         CryptoNodeMetricsResponse metrics = CryptoNodeMetricsResponse.create(nodeMetrics, clusterMetrics);
 
-
         return Response.ok(
-                CryptoResponse.create(metrics, fingerprints, uniqueFingerprints.size() == 1)
+                CryptoResponse.create(metrics, fingerprints, nzyme.getCrypto().allPGPKeysEqualAcrossCluster())
         ).build();
     }
 

src/main/java/app/nzyme/core/MockNzyme.java
@@ -19,6 +19,7 @@ package app.nzyme.core;
 
 import app.nzyme.core.distributed.ClusterManager;
 import app.nzyme.core.distributed.NodeManager;
+import app.nzyme.core.monitoring.health.HealthMonitor;
 import app.nzyme.plugin.Database;
 import app.nzyme.plugin.NodeIdentification;
 import app.nzyme.plugin.Registry;
@@ -204,6 +205,11 @@ public class MockNzyme implements NzymeNode {
 
     @Override
     public ClusterManager getClusterManager() {
+        return null;
+    }
+
+    @Override
+    public HealthMonitor getHealthMonitor() {
         return null;
     }
 

src/main/java/app/nzyme/core/NzymeNode.java
@@ -19,6 +19,7 @@ package app.nzyme.core;
 
 import app.nzyme.core.distributed.ClusterManager;
 import app.nzyme.core.distributed.NodeManager;
+import app.nzyme.core.monitoring.health.HealthMonitor;
 import app.nzyme.plugin.*;
 import app.nzyme.plugin.retro.RetroService;
 import com.codahale.metrics.MetricRegistry;
@@ -56,6 +57,8 @@ public interface NzymeNode extends RemoteConnector, PluginEntryPoint, DatabasePr
 
     NodeManager getNodeManager();
     ClusterManager getClusterManager();
+
+    HealthMonitor getHealthMonitor();
 
     Ethernet getEthernet();
 

src/main/java/app/nzyme/core/NzymeNodeImpl.java
@@ -19,6 +19,7 @@ package app.nzyme.core;
 
 import app.nzyme.core.distributed.ClusterManager;
 import app.nzyme.core.distributed.NodeManager;
+import app.nzyme.core.monitoring.health.HealthMonitor;
 import app.nzyme.core.periodicals.distributed.NodeUpdater;
 import app.nzyme.core.rest.resources.system.cluster.NodesResource;
 import app.nzyme.plugin.Database;
@@ -178,6 +179,8 @@ public class NzymeNodeImpl implements NzymeNode {
     private final ContactManager contactManager;
     private final TrackerManager trackerManager;
 
+    private final HealthMonitor healthMonitor;
+
     private final Anonymizer anonymizer;
 
     private GroundStation groundStation;
@@ -236,6 +239,8 @@ public class NzymeNodeImpl implements NzymeNode {
         this.sentry = new Sentry(this, 5);
         this.clients = new Clients(this);
         this.objectMapper = new ObjectMapper();
+
+        this.healthMonitor = new HealthMonitor(this);
 
         this.deauthenticationMonitor = new DeauthenticationMonitor(this);
 
@@ -362,6 +367,8 @@ public class NzymeNodeImpl implements NzymeNode {
             periodicalManager.scheduleAtFixedRate(new SignalTrackMonitor(this), 60, 60, TimeUnit.SECONDS);
         }
 
+        healthMonitor.initialize();
+
         // Load plugins.
         PluginLoader pl = new PluginLoader(new File(configuration.pluginDirectory())); // TODO make path configurable
         for (Plugin plugin : pl.loadPlugins()) {
@@ -414,6 +421,7 @@ public class NzymeNodeImpl implements NzymeNode {
         resourceConfig.register(CryptoResource.class);
         resourceConfig.register(MonitoringResource.class);
         resourceConfig.register(NodesResource.class);
+        resourceConfig.register(HealthResource.class);
 
         // Plugin-supplied REST resources.
         for (Object resource : pluginRestResources) {
@@ -527,6 +535,11 @@ public class NzymeNodeImpl implements NzymeNode {
     @Override
     public ClusterManager getClusterManager() {
         return clusterManager;
+    }
+
+    @Override
+    public HealthMonitor getHealthMonitor() {
+        return healthMonitor;
     }
 
     private void initializeProbes() {

src/main/java/app/nzyme/core/crypto/Crypto.java
@@ -1,9 +1,11 @@
 package app.nzyme.core.crypto;
 
+import app.nzyme.core.rest.responses.crypto.PGPKeyResponse;
 import app.nzyme.plugin.Database;
 import com.codahale.metrics.Timer;
 import app.nzyme.core.NzymeNode;
 import app.nzyme.core.util.MetricNames;
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
@@ -301,6 +303,15 @@ public class Crypto {
                         .mapTo(String.class)
                         .one()
         );
+    }
+
+    public boolean allPGPKeysEqualAcrossCluster() {
+        List<String> uniqueFingerprints = Lists.newArrayList();
+        for (PGPKeyFingerprint fp : getPGPKeysByNode()) {
+            uniqueFingerprints.add(fp.fingerprint());
+        }
+
+        return uniqueFingerprints.size() == 1;
     }
 
     private PGPPublicKey readPublicKey(File file) throws IOException, PGPException {

src/main/java/app/nzyme/core/database/DatabaseImpl.java
@@ -4,6 +4,7 @@ import app.nzyme.core.configuration.node.NodeConfiguration;
 import app.nzyme.core.distributed.database.NodeEntryMapper;
 import app.nzyme.core.distributed.database.metrics.GaugeHistogramBucketMapper;
 import app.nzyme.core.distributed.database.metrics.TimerSnapshotMapper;
+import app.nzyme.core.monitoring.health.db.IndicatorStatusMapper;
 import app.nzyme.plugin.Database;
 import app.nzyme.core.alerts.service.AlertDatabaseEntryMapper;
 import app.nzyme.core.bandits.database.*;
@@ -94,7 +95,8 @@ public class DatabaseImpl implements Database {
                 .registerRowMapper(new PGPKeyFingerprintMapper())
                 .registerRowMapper(new NodeEntryMapper())
                 .registerRowMapper(new GaugeHistogramBucketMapper())
-                .registerRowMapper(new TimerSnapshotMapper());
+                .registerRowMapper(new TimerSnapshotMapper())
+                .registerRowMapper(new IndicatorStatusMapper());
 
         // Run migrations against underlying JDBC connection.
         JdbcConnection connection = new JdbcConnection(jdbi.open().getConnection());

src/main/java/app/nzyme/core/monitoring/exporters/prometheus/PrometheusFormatter.java
@@ -1,4 +1,4 @@
-package app.nzyme.core.monitoring.prometheus;
+package app.nzyme.core.monitoring.exporters.prometheus;
 
 import com.codahale.metrics.*;
 import org.apache.logging.log4j.LogManager;

src/main/java/app/nzyme/core/monitoring/exporters/prometheus/PrometheusRegistryKeys.java
@@ -1,4 +1,4 @@
-package app.nzyme.core.monitoring.prometheus;
+package app.nzyme.core.monitoring.exporters.prometheus;
 
 import app.nzyme.plugin.RegistryKey;
 import app.nzyme.plugin.rest.configuration.ConfigurationEntryConstraint;

commit b222f515fe1cafaf665d2efdd9be6002ce6f52f9

Fix PGP key list, broken in eca926a

src/main/java/app/nzyme/core/rest/resources/system/CryptoResource.java
@@ -1,5 +1,6 @@
 package app.nzyme.core.rest.resources.system;
 
+import app.nzyme.core.crypto.PGPKeyFingerprint;
 import app.nzyme.core.distributed.MetricExternalName;
 import app.nzyme.core.distributed.database.metrics.TimerSnapshot;
 import app.nzyme.core.rest.responses.crypto.CryptoNodeMetricsResponse;
@@ -37,6 +38,9 @@ public class CryptoResource {
     @Path("summary")
     public Response summary() {
         Map<String, PGPKeyResponse> fingerprints = Maps.newHashMap();
+        for (PGPKeyFingerprint fp : nzyme.getCrypto().getPGPKeysByNode()) {
+            fingerprints.put(fp.node(), PGPKeyResponse.create(fp.node(), fp.fingerprint(), fp.createdAt()));
+        }
 
         Map<UUID, TimerSnapshot> encryption = nzyme.getClusterManager().findMetricTimer(
                 MetricExternalName.PGP_ENCRYPTION_TIMER.database_label