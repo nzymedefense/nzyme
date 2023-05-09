6dd24b5b2429475fb42f7f783d1a85e1adc4546c
Lennart Koopmann <lennart@0x58ed.com>
2023-05-09 18:09:21 -0500

    Handle NULL flag codes better

--- web-interface/src/components/misc/Flag.jsx
+++ web-interface/src/components/misc/Flag.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 function Flag(props) {
 
-  const code = props.code.toLowerCase();
+  const code = props.code;
+
+  if (!code) {
+    return null;
+  }
 
   return (
-      <span className={"fi fi-" + code + " flag-inline"} />
+      <span className={"fi fi-" + code.toLowerCase() + " flag-inline"} />
   )
 
 }
