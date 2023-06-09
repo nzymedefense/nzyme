86d098a4ed789054525057dab3cd21f11c72c0a5
Lennart Koopmann <lennart@0x58ed.com>
2023-06-09 18:38:17 -0500

    802.11 parsing work #815

--- tap/src/brokers/dot11_broker.rs
+++ tap/src/brokers/dot11_broker.rs
@@ -1,9 +1,11 @@
 use std::{sync::Arc, thread};
 
+use anyhow::{Error, bail};
+use bitvec::{view::BitView, order::Lsb0};
 use byteorder::{ByteOrder, LittleEndian};
-use log::{info, debug};
+use log::{info, debug, warn};
 
-use crate::{messagebus::bus::Bus, dot11::frames::Dot11Frame};
+use crate::{messagebus::bus::Bus, dot11::frames::{Dot11Frame, RadiotapHeader, RadiotapHeaderPresentFlags}};
 
 pub struct Dot11Broker {
     num_threads: usize,
@@ -43,14 +45,86 @@ impl Dot11Broker {
 
         let header_length = LittleEndian::read_u16(&data.data[2..4]) as usize;
 
+        if (header_length < 14) {
+            debug!("Received WiFi frame header is too short.");
+            return;
+        }
+
         if data.data.len() < header_length {
             debug!("Received WiFi frame shorter than reported header length.");
             return;
         }
 
-        let radiotap_header = &data.data[0..header_length];
+        // Parse Radiotap header.
+        let header_data = &data.data[0..header_length];
 
-        info!("{:?}", radiotap_header);
+        // Present flags bitmask.
+        let present_flags = match Self::parse_present_flags(&header_data[4..8]) {
+            Ok(flags) => flags,
+            Err(e) => {
+                warn!("Could not parse radiotap present flag bitmask: {}", e);
+                return
+            }
+        };
+        
+        // Data Rate.
+        let data_rate: Option<u16> = if present_flags.rate {
+            Option::Some(((header_data[9] as u16)*500) as u16)
+        } else {
+            Option::None
+        };
+
+        let radiotap = RadiotapHeader { 
+            data_rate
+        };
+
+        info!("{:?}", radiotap);
+    }
+
+    fn parse_present_flags(word: &[u8]) -> Result<RadiotapHeaderPresentFlags, Error> {
+        if word.len() != 4 {
+            bail!("Radiotap present flags must be 4 bytes, provided <{}>.", word.len())
+        }
+
+        let bmask = word.view_bits::<Lsb0>();
+
+        /*
+         * We are using unwrap() here because the bounds length 
+         * above ensures enough bits for the access ops.
+         */
+
+        Ok(RadiotapHeaderPresentFlags { 
+            tsft: *bmask.get(0).unwrap(),
+            flags: *bmask.get(1).unwrap(),
+            rate: *bmask.get(2).unwrap(),
+            channel: *bmask.get(3).unwrap(),
+            fhss: *bmask.get(4).unwrap(),
+            antenna_signal_dbm: *bmask.get(5).unwrap(),
+            antenna_noise_dbm: *bmask.get(6).unwrap(),
+            lock_quality: *bmask.get(7).unwrap(),
+            tx_attenuation: *bmask.get(8).unwrap(),
+            tx_attenuation_db: *bmask.get(9).unwrap(),
+            tx_power_dbm: *bmask.get(10).unwrap(),
+            antenna: *bmask.get(11).unwrap(),
+            antenna_signal_db: *bmask.get(12).unwrap(),
+            antenna_noise_db: *bmask.get(13).unwrap(),
+            rx_flags: *bmask.get(14).unwrap(),
+            tx_flags: *bmask.get(15).unwrap(),
+            data_retries: *bmask.get(16).unwrap(),
+            channel_plus: *bmask.get(17).unwrap(),
+            mcs: *bmask.get(18).unwrap(),
+            ampdu: *bmask.get(19).unwrap(),
+            vht: *bmask.get(20).unwrap(),
+            timestamp: *bmask.get(21).unwrap(),
+            he_info: *bmask.get(22).unwrap(),
+            hemu_info: *bmask.get(23).unwrap(),
+            zero_length_psdu: *bmask.get(24).unwrap(),
+            lsig: *bmask.get(25).unwrap(),
+            tlvs: *bmask.get(26).unwrap(),
+            radiotap_ns_next: *bmask.get(27).unwrap(),
+            vendor_nx_next: *bmask.get(28).unwrap(),
+            ext: *bmask.get(29).unwrap()
+        })
     }
 
 }
--- tap/src/dot11/frames.rs
+++ tap/src/dot11/frames.rs
@@ -1,9 +1,48 @@
 #[derive(Debug)]
-
 pub struct Dot11Frame {
     pub data: Vec<u8>
 }
 
+#[derive(Debug)]
+pub struct RadiotapHeader {
+    pub data_rate: Option<u16>
+}
+
+#[derive(Debug)]
+pub struct RadiotapHeaderPresentFlags {
+    pub tsft: bool,
+    pub flags: bool,
+    pub rate: bool,
+    pub channel: bool,
+    pub fhss: bool,
+    pub antenna_signal_dbm: bool,
+    pub antenna_noise_dbm: bool,
+    pub lock_quality: bool,
+    pub tx_attenuation: bool,
+    pub tx_attenuation_db: bool,
+    pub tx_power_dbm: bool,
+    pub antenna: bool,
+    pub antenna_signal_db: bool,
+    pub antenna_noise_db: bool,
+    pub rx_flags: bool,
+    pub tx_flags: bool,
+    pub data_retries: bool,
+    pub channel_plus: bool,
+    pub mcs: bool,
+    pub ampdu: bool,
+    pub vht: bool,
+    pub timestamp: bool,
+    pub he_info: bool,
+    pub hemu_info: bool,
+    pub zero_length_psdu: bool,
+    pub lsig: bool,
+    pub tlvs: bool,
+    pub radiotap_ns_next: bool,
+    pub vendor_nx_next: bool,
+    pub ext: bool
+}
+
+#[derive(Debug)]
 pub struct Dot11ManagementFrame {
     pub header: Vec<u8>,
     pub payload: Vec<u8>,
