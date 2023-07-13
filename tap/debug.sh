58c00195fea537ddd2144231ea2af2eaf935b08d
Lennart Koopmann <lennart@0x58ed.com>
2023-07-13 12:58:32 -0500

    Make scripts executable by default

--- tap/debug.sh
+++ tap/debug.sh
@@ -12,4 +12,4 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-RUST_LOG=debug RUST_BACKTRACE=1 target/debug/nzyme-tap
+RUST_LOG=debug RUST_BACKTRACE=1 target/debug/nzyme-tap --configuration-file nzyme-tap.conf --log-level debug
