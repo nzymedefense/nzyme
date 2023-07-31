9efe0c889394b9b7930aeb32343330e4234bfa42
Lennart Koopmann <lennart@0x58ed.com>
2023-07-31 18:25:52 -0500

    Updated/new help links #722

--- web-interface/src/components/dot11/monitoring/Dot11MonitoringPage.jsx
+++ web-interface/src/components/dot11/monitoring/Dot11MonitoringPage.jsx
@@ -7,8 +7,12 @@ function Dot11MonitoringPage() {
   return (
     <React.Fragment>
       <div className="row">
-        <div className="col-md-12">
+        <div className="col-md-10">
           <h1>Monitored Networks</h1>
+        </div>
+
+        <div className="col-md-2">
+          <a href="https://go.nzyme.org/wifi-network-monitoring" className="btn btn-secondary float-end">Help</a>
         </div>
       </div>
 
