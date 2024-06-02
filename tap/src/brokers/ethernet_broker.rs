bc7e71907b43f4fe2d5bb72a84b94843365c283d
Lennart Koopmann <lennart.koopmann@nzyme.org>
2024-06-02 14:15:14 -0500

    Support 802.1Q #1053

--- tap/src/brokers/ethernet_broker.rs
+++ tap/src/brokers/ethernet_broker.rs
@@ -2,12 +2,12 @@ use std::panic::catch_unwind;
 use std::{thread, sync::Arc};
 
 use chrono::Utc;
-use log::{debug, info, warn, error};
+use log::{debug, info, warn, error, trace};
 use byteorder::{BigEndian, ByteOrder};
 use crate::ethernet::packets::EthernetData;
 use crate::ethernet::parsers::{udp_parser, tcp_parser};
 use crate::messagebus::bus::Bus;
-use crate::to_pipeline;
+use crate::{to_pipeline, tracemark};
 use crate::{helpers::network::to_mac_address_string, ethernet::types};
 use crate::ethernet::{
     packets::EthernetPacket,
@@ -52,17 +52,45 @@ impl EthernetBroker {
 
     #[allow(clippy::cast_possible_truncation)]
     fn handle(data: &Arc<EthernetData>, bus: &Arc<Bus>) {
+        if data.data.len() < 14 {
+            tracemark!("Ethernet data too short.");
+            return;
+        }
+
         let packet_type = BigEndian::read_u16(&data.data[12..14]);
         let len = data.data.len();
+
+        /*
+         * If this is 802.1Q ("VLAN"), we have to parse the extra data, move the cursor ahead of it
+         * and then overwrite the packet type with the original packet type to continue parsing.
+         */
+        let (packet_type, data_cursor) = if packet_type == 0x8100 {
+            if data.data.len() < 18 {
+                tracemark!("802.1Q data too short.");
+                return;
+            }
+
+            // If we ever wanted to parse VLAN info, we'd do that here.
+
+            (BigEndian::read_u16(&data.data[16..18]), 18)
+        } else {
+            // Not 802.1Q, leave unchanged.
+            (packet_type, 14)
+        };
+
+        if data.data.len() < data_cursor {
+            tracemark!("Ethernet data too short.");
+            return;
+        }
 
         let packet = Arc::new(EthernetPacket {
             destination_mac: to_mac_address_string(&data.data[0..6]),
             source_mac: to_mac_address_string(&data.data[6..12]),
-            data: data.data[14..len].to_vec(),
+            data: data.data[data_cursor..].to_vec(),
             packet_type: find_ethertype(packet_type),
             size: len as u32,
             timestamp: Utc::now()
-        }); 
+        });
 
         if packet.packet_type == EtherType::IPv4 {
             let ipv4 = match ipv4_parser::parse(&packet) {
@@ -123,6 +151,7 @@ impl EthernetBroker {
                 Err(e) => { warn!("Could not parse ARP packet: {}", e) }
             };
         } else {
+            debug!("Unknown packet type {}", packet_type)
         }
     }
 
