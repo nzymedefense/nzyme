532af7f95009e3acbd3b9da7400caeca4e6cf74d
Lennart Koopmann <lennart@0x58ed.com>
2024-07-30 21:16:16 -0500

    Make L4Address work when underlying connection is missing #1055

--- web-interface/src/components/ethernet/shared/L4Address.jsx
+++ web-interface/src/components/ethernet/shared/L4Address.jsx
@@ -6,6 +6,15 @@ export default function L4Address(props) {
   const address = props.address;
   const hidePort = props.hidePort;
 
+  if (!address) {
+    return (
+        <span>
+          [missing] <i className="fa-solid fa-circle-question"
+                       title="Underlying connection information has been retention cleaned or not recorded."></i>
+        </span>
+    )
+  }
+
   return (
       <IPAddressLink ip={address.address} port={hidePort ? null : address.port} />
   )
