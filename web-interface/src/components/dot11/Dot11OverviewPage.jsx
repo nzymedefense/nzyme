3df5441d02a2bafac0111f8a29706f9c750cb095
Lennart Koopmann <lennart.koopmann@nzyme.org>
2024-10-18 21:19:45 -0500

    Conditionally load elements on 802.11 overview page #843

--- web-interface/src/components/dot11/Dot11OverviewPage.jsx
+++ web-interface/src/components/dot11/Dot11OverviewPage.jsx
@@ -1,19 +1,26 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import DetectionAlertsService from "../../services/DetectionAlertsService";
 import LoadingSpinner from "../misc/LoadingSpinner";
 import NumberCard from "../widgets/presentation/NumberCard";
+import {userHasPermission} from "../../util/Tools";
+import {UserContext} from "../../App";
+import WithPermission from "../misc/WithPermission";
 
 const alertsService = new DetectionAlertsService();
 
 export default function Dot11OverviewPage() {
 
+  const user = useContext(UserContext);
+
   const [alerts, setAlerts] = useState(null);
 
   useEffect(() => {
-    alertsService.findAllAlerts(setAlerts, 10, 0, "DOT11");
+    if (userHasPermission(user, "alerts_view")) {
+      alertsService.findAllAlerts(setAlerts, 10, 0, "DOT11");
+    }
   }, []);
 
-  if (!alerts) {
+  if (userHasPermission(user, "alerts_view") && !alerts) {
     return <LoadingSpinner />
   }
 
@@ -25,11 +32,13 @@ export default function Dot11OverviewPage() {
         </div>
       </div>
 
-      <div className="row mt-3">
-        <div className="col-6">
-          <NumberCard title="Active Alerts" value={alerts.total_active} numberFormat="0,0"/>
+      <WithPermission permission="alerts_view">
+        <div className="row mt-3">
+          <div className="col-6">
+            <NumberCard title="Active Alerts" value={alerts.total_active} numberFormat="0,0"/>
+          </div>
         </div>
-      </div>
+      </WithPermission>
     </React.Fragment>
   )
 
