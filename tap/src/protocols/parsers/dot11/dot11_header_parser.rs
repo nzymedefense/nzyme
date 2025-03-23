422c3224b72ac8830cd39b7b5bec7f1cb54e0941
Adriano Maia <adriano@drstrange.wtf>
2025-03-23 12:19:22 -0500

    - first parser improvements based on fuzzing feedback

--- tap/src/protocols/parsers/dot11/dot11_header_parser.rs
+++ tap/src/protocols/parsers/dot11/dot11_header_parser.rs
@@ -4,6 +4,7 @@ use bitvec::order::Lsb0;
 use bitvec::view::BitView;
 use byteorder::{ByteOrder, LittleEndian};
 use log::{trace, warn};
+use crate::helpers::buffer_cursor::BufferCursor;
 use crate::helpers::network::dot11_frequency_to_channel;
 use crate::wireless::dot11::frames::{Dot11RawFrame, Dot11Frame, RadiotapHeader, RadiotapHeaderPresentFlags, RadiotapHeaderFlags, FrameTypeInformation, FrameType, FrameSubType};
 
@@ -16,41 +17,53 @@ pub fn parse(data: &Arc<Dot11RawFrame>) -> Result<(Dot11Frame, u32), Error> {
 
     let header_length = LittleEndian::read_u16(&data.data[2..4]) as usize;
 
-    if data.data.len() < header_length {
+    if header_length <= 0 {
+        trace!("Header length is 0 or negative. Malformed radiotap header.");
+        bail!("Malformed radiotap header.");
+    }
+
+    // check if we have enough data (minus the first 4 bytes we already consumed) to read a header
+    if header_length < 8 || data.data.len() < header_length {
         trace!("Received WiFi frame shorter than reported header length.");
-        bail!("WiFi frame shorter than reported header length");
+        bail!("WiFi frame data too short.");
     }
 
-    // Parse Radiotap header.
-    let header_data = &data.data[0..header_length];
+    // Load the radiotap buffer according to the length on the header.
+    let mut radiotap_buffer = BufferCursor::new(&data.data[0..header_length]);
 
-    if data.data.len() < 8 {
-        trace!("Received WiFi frame too short to fit present flags and anything else.");
-        bail!("WiFi frame too short");
-    }
-
-    // Present flags bitmask.
-    let (ca, present_flags) = match parse_present_flags(&header_data[4..header_data.len()]) {
-        Ok((cursor_advance, flags)) => (cursor_advance, flags),
-        Err(e) => {
-            warn!("Could not parse radiotap present flag bitmask: {}", e);
-            bail!("Could not parse present flag bitmask");
-        }
+    // Advance the first four bytes of the buffer: version, pad and length. Already parsed.
+    let _ = match radiotap_buffer.take(4) {
+        Some(a) => a,
+        None => bail!("Radiotap buffer length is less than 4 bytes."),
     };
 
-    let mut cursor = 4+ca;
+    // Present flags bitmask
+    let present_flags = match parse_present_flags(&mut radiotap_buffer) {
+        Ok(flags) => flags,
+        Err(e) => {
+            bail!("Could not parse present flags bitmask: {}", e);
+        }
+    };
 
     // Variable fields follow. Not always set.
 
     // TSFT.
     if present_flags.tsft {
         // not parsing
-        cursor += 8;
+        let _ = match radiotap_buffer.take(8) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data to parse."),
+        };
     }
 
     // Flags.
     let flags = if present_flags.flags {
-        let frame_flags = parse_flags(&header_data[cursor]);
+        let frame_flags = match parse_flags(&mut radiotap_buffer) {
+            Ok(flags) => flags,
+            Err(e) => {
+                bail!("Could not parse flags bitmask: {}", e);
+            }
+        };
 
         // Abort if checksum check failed. (Driver should not have passed this along.)
         if frame_flags.bad_fcs {
@@ -58,32 +71,32 @@ pub fn parse(data: &Arc<Dot11RawFrame>) -> Result<(Dot11Frame, u32), Error> {
             bail!("Could not parse bad FCS frame from interface [{}]", data.interface_name);
         }
 
-        cursor += 1;
-
         Some(frame_flags)
     } else {
-        cursor += 1;
         None
     };
 
     // Data Rate.
-    let data_rate: Option<u16> = if present_flags.rate {
-        if header_data.len() <= cursor {
-            bail!("WiFi frame is too short. [{:?}]", data);
-        }
-
-        let data_rate = (header_data[cursor] as u16)*500;
-        cursor += 1;
-
+    let data_rate: Option<u32> = if present_flags.rate {
+        let data_rate = match radiotap_buffer.take(1) {
+            Some(a) => (a[0] as u32)*500,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
+        
         Some(data_rate)
     } else {
-        cursor += 1;
         None
     };
 
     // Channel.
     let frequency = if present_flags.channel {
-        let frequency = LittleEndian::read_u16(&data.data[cursor..cursor+2]);
+        let frequency_data = match radiotap_buffer.take(2) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
+        
+        
+        let frequency = LittleEndian::read_u16(frequency_data);
 
         if frequency == 0 {
             trace!("Filtering out frame with bad reported frequency on [{}].",
@@ -91,80 +104,94 @@ pub fn parse(data: &Arc<Dot11RawFrame>) -> Result<(Dot11Frame, u32), Error> {
             bail!("Could not parse bad frame with bad reported frequency");
         }
 
-        cursor += 2;
-
         // Skip the channel flags. Not parsing.
-        cursor += 2;
+        let _ = match radiotap_buffer.take(2) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
 
         Some(frequency)
     } else {
-        cursor += 4;
         None
     };
 
     // FHSS.
     if present_flags.fhss {
         // not parsing
-        cursor += 2;
+        let _ = match radiotap_buffer.take(2) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
     }
 
     // Antenna signal (dBm)
     let antenna_signal = if present_flags.antenna_signal_dbm {
-        if header_data.len() <= cursor {
-            bail!("WiFi frame is too short. [{:?}]", data);
-        }
+        let dbm_data = match radiotap_buffer.take(1) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
 
-        let dbm = header_data[cursor] as i8;
-        cursor += 1;
-
+        let dbm = dbm_data[0] as i8;
         Some(dbm)
     } else {
-        cursor += 1;
         None
     };
 
     // Antenna noise (dBm)
     if present_flags.antenna_noise_dbm {
         // not parsing
-        cursor += 1;
+        let _ = match radiotap_buffer.take(1) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
     }
 
     // Lock quality.
     if present_flags.lock_quality {
         // not parsing
-        cursor += 2;
+        let _ = match radiotap_buffer.take(2) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
     }
 
     // TX attenuation.
     if present_flags.tx_attenuation {
         // not parsing
-        cursor += 1;
+        let _ = match radiotap_buffer.take(1) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
     }
 
     // TX attenuation (dB)
     if present_flags.tx_attenuation_db {
         // not parsing
-        cursor += 2;
+        let _ = match radiotap_buffer.take(2) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
     }
 
     // TX power (dBm)
     if present_flags.tx_power_dbm {
         // not parsing
-        cursor += 1;
+        let _ = match radiotap_buffer.take(1) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
     }
 
     // Antenna.
     let antenna = if present_flags.antenna {
-        if header_data.len() <= cursor {
-            bail!("WiFi frame is too short. [{:?}]", data);
-        }
-
-        let antenna = header_data[cursor];
-        cursor += 1;
-
+        let antenna_data = match radiotap_buffer.take(1) {
+            Some(a) => a,
+            None => bail!("Radiotap buffer is malformed, not enough data."),
+        };
+        
+        let antenna = antenna_data[0];
         Some(antenna)
     } else {
-        cursor += 1;
         None
     };
 
@@ -195,7 +222,7 @@ pub fn parse(data: &Arc<Dot11RawFrame>) -> Result<(Dot11Frame, u32), Error> {
         antenna
     };
 
-    let payload = &data.data[header_length..data.data.len()];
+    let payload = radiotap_buffer.slice_from_current();
 
     if payload.len() < 2 {
         trace!("Payload too short.");
@@ -212,11 +239,14 @@ pub fn parse(data: &Arc<Dot11RawFrame>) -> Result<(Dot11Frame, u32), Error> {
     }, payload.len() as u32))
 }
 
-fn parse_present_flags(mask: &[u8]) -> Result<(usize, RadiotapHeaderPresentFlags), Error> {
-    assert!(mask.len() >= 4);
+fn parse_present_flags(radiotap_buffer: &mut BufferCursor) -> Result<RadiotapHeaderPresentFlags, Error> {
 
-    let mut flags_cursor = 4;
-    let bmask = mask[0..flags_cursor].view_bits::<Lsb0>();
+    let present_buffer = match radiotap_buffer.take(4) {
+        Some(buffer) => buffer,
+        None => bail!("Radiotap buffer is too short")
+    };
+
+    let bmask = present_buffer.view_bits::<Lsb0>();
 
     let first_flags = RadiotapHeaderPresentFlags {
         tsft: *bmask.get(0).unwrap(),
@@ -235,56 +265,64 @@ fn parse_present_flags(mask: &[u8]) -> Result<(usize, RadiotapHeaderPresentFlags
         antenna_noise_db: *bmask.get(13).unwrap(),
         rx_flags: *bmask.get(14).unwrap(),
         tx_flags: *bmask.get(15).unwrap(),
-        data_retries: *bmask.get(16).unwrap(),
-        channel_plus: *bmask.get(17).unwrap(),
-        mcs: *bmask.get(18).unwrap(),
-        ampdu: *bmask.get(19).unwrap(),
-        vht: *bmask.get(20).unwrap(),
-        timestamp: *bmask.get(21).unwrap(),
-        he_info: *bmask.get(22).unwrap(),
-        hemu_info: *bmask.get(23).unwrap(),
-        zero_length_psdu: *bmask.get(24).unwrap(),
-        lsig: *bmask.get(25).unwrap(),
-        tlvs: *bmask.get(26).unwrap(),
-        radiotap_ns_next: *bmask.get(27).unwrap(),
-        vendor_nx_next: *bmask.get(28).unwrap(),
-        ext: *bmask.get(29).unwrap()
+        rts_retries: *bmask.get(16).unwrap(),
+        data_retries: *bmask.get(17).unwrap(),
+        channel_plus: *bmask.get(18).unwrap(),
+        mcs: *bmask.get(19).unwrap(),
+        ampdu: *bmask.get(20).unwrap(),
+        vht: *bmask.get(21).unwrap(),
+        timestamp: *bmask.get(22).unwrap(),
+        he_info: *bmask.get(23).unwrap(),
+        hemu_info: *bmask.get(24).unwrap(),
+        hemu_user_info: *bmask.get(25).unwrap(),
+        zero_length_psdu: *bmask.get(26).unwrap(),
+        lsig: *bmask.get(27).unwrap(),
+        tlvs: *bmask.get(28).unwrap(),
+        radiotap_ns_next: *bmask.get(29).unwrap(),
+        vendor_nx_next: *bmask.get(30).unwrap(),
+        ext: *bmask.get(31).unwrap()
     };
 
     if first_flags.ext {
         loop {
             /*
              * Some Wi-Fi adapters will have multiple sets of present flags, indicated by
-             * the `ext` set to true. It is currently unclear why. Best way here appears to be
+             * the `ext` set to true. Mainly extensions or vendor specific flags. Best way here appears to be
              * using the first set of flags, ignore any that follows, but sufficiently advance
              * the cursor to resume reading of trailing information in the frame.
              */
 
-            if mask.len() < flags_cursor+4 {
-                bail!("Invalid flags length {}, cursor: {}", mask.len(), flags_cursor);
-            }
+            let next_present = match radiotap_buffer.take(4) {
+                Some(buffer) => buffer,
+                None => bail!("Radiotap buffer is too short")
+            };
 
-            let next_mask = mask[flags_cursor..flags_cursor+4].view_bits::<Lsb0>();
-            flags_cursor += 4;
+            let next_mask = next_present.view_bits::<Lsb0>();
 
-            if !*next_mask.get(29).unwrap() {
+            if !*next_mask.get(31).unwrap() {
                 break;
             }
         }
     }
 
-    Ok((flags_cursor, first_flags))
+    Ok(first_flags)
 }
 
-fn parse_flags(mask: &u8) -> RadiotapHeaderFlags {
-    let bmask = mask.view_bits::<Lsb0>();
+fn parse_flags(radiotap_buffer: &mut BufferCursor) -> Result<RadiotapHeaderFlags, Error> {
+    let data = match radiotap_buffer.take(1) {
+        Some(buffer) => buffer,
+        None => bail!("Radiotap buffer is too short")
+    };
+    
+    
+    let bmask = data.view_bits::<Lsb0>();
 
     /*
      * We are using unwrap() here because the passed u8
      * ensures enough bits for the access ops.
      */
 
-    RadiotapHeaderFlags {
+    Ok(RadiotapHeaderFlags {
         cfp: *bmask.get(0).unwrap(),
         preamble: *bmask.get(1).unwrap(),
         wep: *bmask.get(2).unwrap(),
@@ -293,7 +331,7 @@ fn parse_flags(mask: &u8) -> RadiotapHeaderFlags {
         data_pad: *bmask.get(5).unwrap(),
         bad_fcs: *bmask.get(6).unwrap(),
         short_gi: *bmask.get(7).unwrap(),
-    }
+    })
 }
 
 fn parse_frame_type(mask: &u8) -> FrameTypeInformation {
