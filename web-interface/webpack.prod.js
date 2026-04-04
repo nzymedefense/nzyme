af2572471e50fe2134025c21fce028751638506a
Lennart Koopmann <lennart.koopmann@nzyme.org>
2026-04-03 19:41:20 -0500

    Fix webpack config

--- web-interface/webpack.prod.js
+++ web-interface/webpack.prod.js
@@ -1,19 +1,22 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
-module.exports = merge(common, {
-    bail: true,
-    mode: 'production',
-    plugins: [
-        new WebpackManifestPlugin({
-            fileName: 'asset-manifest.json',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {from: "public/static", to: "static"}
-            ]
-        })
-    ]
-});
+module.exports = async () => {
+    const { WebpackManifestPlugin } = await import('webpack-manifest-plugin');
+
+    return merge(common, {
+        bail: true,
+        mode: 'production',
+        plugins: [
+            new WebpackManifestPlugin({
+                fileName: 'asset-manifest.json',
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {from: "public/static", to: "static"}
+                ]
+            })
+        ]
+    });
+};
